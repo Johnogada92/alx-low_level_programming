e9cdc8c68b2834b60a2160a11449da95c37dc6ac
johnogada92 <johnogada92@gmail.com>
2023-07-14 06:02:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
- * Hello C
- * You can always find time to use C for the common programming practicals
- *
- * It is a a very strong language
- * You do not need alot of code to understand the concept
- */
+ * main - main entry point
+ * return : always 0 (Success)
+ * */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
