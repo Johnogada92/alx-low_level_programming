60dc0da60d062e26725eb5d80091ca687436cfa1
johnogada92 <johnogada92@gmail.com>
2023-07-14 05:52:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /**
- *Hello C World
+ * Hello C
+ * You can always find time to use C for the common programming practicals
+ *
+ * It is a a very strong language
+ * You do not need alot of code to understand the concept
  */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
