6a949490b0fc7027d0c89bb815bae36b6aecec55
johnogada92 <johnogada92@gmail.com>
2023-07-18 11:36:34 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <alx.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -6,10 +6,15 @@
  */
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-	_putchar('l');
-	_putchar('x');
+	_putchar('r');
 	_putchar('\n');
 
-	return 0;
+	return (0);
 }
